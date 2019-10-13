0103a59499341761f37700a733d2b6e73cc203aa
gekoramy <luka.mosetti@gmail.com>
2019-10-13 19:56:44 +0200

    Add exam prescriptions view

---
+++ src/main/java/dunder/mifflin/http/views/specialist/ExamPrescriptions.java
@@ -0,0 +1,60 @@
+package dunder.mifflin.http.views.specialist;
+
+import dunder.mifflin.persistance.daos.exceptions.DAOException;
+import dunder.mifflin.persistance.pojos.*;
+import dunder.mifflin.services.DAOs;
+import dunder.mifflin.utils.Auths;
+import dunder.mifflin.utils.Avatars;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.*;
+
+import static dunder.mifflin.utils.Locations.location;
+import static java.util.stream.Collectors.toUnmodifiableList;
+import static java.util.stream.Collectors.toUnmodifiableSet;
+
+@WebServlet("/specialist/exams")
+public class ExamPrescriptions extends HttpServlet {
+
+    @Inject
+    DAOs daos;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+        try {
+            final long sid = Auths.session(req).orElseThrow();
+            final Specialist specialist = daos.factory().specialist().byKey(sid).orElseThrow();
+            final String avatar = Avatars.avatar50(daos.factory().avatar(), req.getContextPath(), specialist);
+
+            final long pid = Optional.ofNullable(req.getParameter("patient")).map(Long::parseLong).orElseThrow();
+            final Person patient = daos.factory().person().byKey(pid).orElseThrow();
+            final List<ExamPrescription> exams = daos.factory().examPrescription().concerns(patient.id(), "").collect(toUnmodifiableList());
+            final Set<SpExam> qualified = daos.factory().spExam().qualifiedFor(specialist.id()).collect(toUnmodifiableSet());
+
+            final Long[] prescriptions = exams.stream().map(Prescription::id).toArray(Long[]::new);
+            final Map<Long, Ticket> tickets = daos.factory().ticket().byKeys(prescriptions);
+            final Map<Long, Report> reports = daos.factory().report().byKeys(prescriptions);
+
+            req.setAttribute("specialist", specialist);
+            req.setAttribute("avatar", avatar);
+            req.setAttribute("patient", patient);
+            req.setAttribute("exams", exams);
+            req.setAttribute("qualified", qualified);
+            req.setAttribute("tickets", tickets);
+            req.setAttribute("reports", reports);
+            req.getServletContext().getRequestDispatcher("/specialist/exams.jsp").forward(req, resp);
+
+        } catch (NoSuchElementException e) {
+            resp.sendRedirect(location(req, "/login"));
+        } catch (DAOException e) {
+            req.setAttribute("exception", e);
+            resp.sendRedirect(location(req, "/exception"));
+        }
+    }
+}
--- src/main/webapp/specialist/exams.jsp
+++ src/main/webapp/specialist/exams.jsp
@@ -5,8 +5,10 @@
 <jsp:useBean id="specialist" scope="request" type="dunder.mifflin.persistance.pojos.Specialist"/>
 <jsp:useBean id="avatar" scope="request" type="java.lang.String"/>
 <jsp:useBean id="patient" scope="request" type="dunder.mifflin.persistance.pojos.Person"/>
-<jsp:useBean id="medicines" scope="request" type="java.util.List<dunder.mifflin.persistance.pojos.MedicinePrescription>"/>
+<jsp:useBean id="exams" scope="request" type="java.util.List<dunder.mifflin.persistance.pojos.ExamPrescription>"/>
+<jsp:useBean id="qualified" scope="request" type="java.util.Set<dunder.mifflin.persistance.pojos.SpExam>"/>
 <jsp:useBean id="tickets" scope="request" type="java.util.Map<java.lang.Long, dunder.mifflin.persistance.pojos.Ticket>"/>
+<jsp:useBean id="reports" scope="request" type="java.util.Map<java.lang.Long, dunder.mifflin.persistance.pojos.Report>"/>
 
 <html>
 <head>
@@ -41,7 +43,7 @@
     </tr>
 </table>
 
-<form method="get" action="${pageContext.request.contextPath}/specialist/exams">
+<form method="get" action="${pageContext.request.contextPath}/specialist/medicines">
     <input type="submit" name="patient" value="${patient.id()}"/>
 </form>
 
@@ -50,20 +52,18 @@
     <tr>
         <th>Data Prescrizione</th>
         <th>Nome</th>
-        <th>Quantita</th>
         <th>Ricevuta</th>
-        <th>QR code</th>
+        <th>Risultati</th>
     </tr>
     </thead>
-    <c:forEach items="${medicines}" var="it">
+    <c:forEach items="${exams}" var="it">
+        <fmt:parseDate value="${it.date()}" type="date" pattern="yyyy-MM-dd" var="tmp"/>
+        <fmt:formatDate value="${tmp}" type="date" pattern="yyyy/MM/dd" var="out"/>
         <tr>
-            <fmt:parseDate value="${it.date()}" type="date" pattern="yyyy-MM-dd" var="tmp"/>
-            <fmt:formatDate value="${tmp}" type="date" pattern="yyyy/MM/dd" var="out"/>
             <td>${out}</td>
-            <td>${it.medicine().name()}</td>
-            <td>${it.quantity()}</td>
+            <td>${it.exam().name()}</td>
             <td>${tickets.get(it.id())}</td>
-            <td><a href="${pageContext.request.contextPath}/qr?size=500&prescription=${it.id()}" target="_blank">qr</a></td>
+            <td>${reports.get(it.id())}</td>
         </tr>
     </c:forEach>
 </table>
