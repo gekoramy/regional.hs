be605cb452fa86b49c5ad6213d3bd05fa7befde3
gekoramy <luka.mosetti@gmail.com>
2019-11-11 12:10:06 +0100

    add qualified doctors information

--- src/main/java/dunder/mifflin/http/api/Exams.java
+++ src/main/java/dunder/mifflin/http/api/Exams.java
@@ -4,6 +4,8 @@ import com.github.cliftonlabs.json_simple.JsonArray;
 import dunder.mifflin.beans.DAOs;
 import dunder.mifflin.persistence.daos.exceptions.DAOException;
 import dunder.mifflin.persistence.pojos.Examination;
+import dunder.mifflin.persistence.pojos.Person;
+import dunder.mifflin.utils.Avatars;
 import dunder.mifflin.utils.Jsonify;
 import org.apache.commons.codec.CharEncoding;
 
@@ -14,8 +16,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
-import java.util.Optional;
+import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @WebServlet("/api/exams")
@@ -31,9 +34,16 @@ public class Exams extends HttpServlet {
 
             final Stream<Examination> hs = daos.factory().hsExam().contains(pattern).map(Function.identity());
             final Stream<Examination> sp = daos.factory().spExam().contains(pattern).map(Function.identity());
+            final List<Examination> exams = Stream.concat(hs, sp).sorted(Comparator.comparing(Examination::name)).collect(Collectors.toUnmodifiableList());
+            final Map<Long, List<Person>> qualified = daos.factory().person().qualifiedFor(exams.stream().map(Examination::id).toArray(Long[]::new));
+            final Map<Long, String> avatars = Avatars.avatars50(
+                    daos.factory().avatar(),
+                    req.getContextPath(),
+                    qualified.values().stream().flatMap(Collection::stream).collect(Collectors.toUnmodifiableList())
+            );
 
-            final JsonArray array = Stream.concat(hs, sp)
-                    .map(Jsonify::json)
+            final JsonArray array = exams.stream()
+                    .map((exam) -> Jsonify.json(exam, qualified.get(exam.id()), avatars))
                     .collect(Jsonify.array());
 
             resp.setCharacterEncoding(CharEncoding.UTF_8);
